27f827de63e0b7cf2146a234950d709524e4a0fa
Alban <125637@supinfo.com>
2014-05-01 01:22:42 +0200

    Ajout des require des modèles ajoutés.

--- v2/required.php
+++ v2/required.php
@@ -11,8 +11,12 @@ $projectRoot = $_SERVER['DOCUMENT_ROOT'].'/eggstar/v2';
 require_once '/Interfaces/AccountManager.interface.php';
 require_once '/Interfaces/RefPlanManager.interface.php';
 require_once '/Interfaces/UserManager.interface.php';
+require_once '/Interfaces/ElementManager.interface.php';
+require_once '/Interfaces/RefElementManager.interface.php';
 
 require_once '/Managers/AbstractManager.class.php';
 require_once '/Managers/AccountManager.class.php';
 require_once '/Managers/RefPlanManager.class.php';
 require_once '/Managers/UserManager.class.php';
+require_once '/Managers/ElementManager.class.php';
+require_once '/Managers/RefElementManager.class.php';
