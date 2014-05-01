515aa3b453595e5c5746bd79d7fbb548a04f45ea
Alban <125637@supinfo.com>
2014-05-01 14:05:09 +0200

    fonctions de récupération des détails d'éléments. Ajout du keyword public devant function.

--- v2/Managers/ElementManager.class.php
+++ v2/Managers/ElementManager.class.php
@@ -68,7 +68,7 @@ class ElementManager extends AbstractManager implements ElementManagerInterface{
      * @return array
      */
 
-    function find($criteria, $fieldsToReturn = array())
+    public function find($criteria, $fieldsToReturn = array())
     {
         $cursor = parent::__find('element', $criteria, $fieldsToReturn);
 
@@ -99,7 +99,7 @@ class ElementManager extends AbstractManager implements ElementManagerInterface{
      * @return array
      */
 
-    function findOne($criteria, $fieldsToReturn = array())
+    public function findOne($criteria, $fieldsToReturn = array())
     {
         $result = parent::__findOne('element', $criteria, $fieldsToReturn);
         $result = self::convert($result);
@@ -117,7 +117,7 @@ class ElementManager extends AbstractManager implements ElementManagerInterface{
      * @return array contenant le message d'erreur
      */
 
-    function findById($id, $fieldsToReturn = array())
+    public function findById($id, $fieldsToReturn = array())
     {
         $result = parent::__findOne('element', array('_id' => new MongoId($id)));
         $result = self::convert($result);
@@ -134,7 +134,7 @@ class ElementManager extends AbstractManager implements ElementManagerInterface{
      * @return array tableau d'objets Element
      */
 
-    function findAll($fieldsToReturn = array())
+    public function findAll($fieldsToReturn = array())
     {
         $cursor = parent::__find('element', array());
 
@@ -168,7 +168,7 @@ class ElementManager extends AbstractManager implements ElementManagerInterface{
      * @return array
      */
 
-    function findAndModify($searchQuery, $updateCriteria, $fieldsToReturn = NULL, $options = NULL)
+    public function findAndModify($searchQuery, $updateCriteria, $fieldsToReturn = NULL, $options = NULL)
     {
         $result = parent::__findAndModify('element', $searchQuery, $updateCriteria, $fieldsToReturn, $options);
         $result = self::convert($result);
@@ -186,7 +186,7 @@ class ElementManager extends AbstractManager implements ElementManagerInterface{
      * @return TRUE|array contenant le message d'erreur dans un indexe 'error'
      */
 
-    function create($document, $options = array('w' => 1))
+    public function create($document, $options = array('w' => 1))
     {
         $result = parent::__create('element', $document, $options);
 
@@ -203,7 +203,7 @@ class ElementManager extends AbstractManager implements ElementManagerInterface{
      * @return TRUE|array contenant le message d'erreur dans un indexe 'error'
      */
 
-    function update($criteria, $update, $options = array('w' => 1))
+    public function update($criteria, $update, $options = array('w' => 1))
     {
         $result = parent::__update('element', $criteria, $update, $options);
 
@@ -220,10 +220,177 @@ class ElementManager extends AbstractManager implements ElementManagerInterface{
      * @return TRUE|array contenant le message d'erreur dans un indexe 'error'
      */
 
-    function remove($criteria, $options = array('w' => 1))
+    public function remove($criteria, $options = array('w' => 1))
     {
         $result = parent::__remove('element', $criteria, $options);
 
         return $result;
+    }
+
+    /**
+     * - Distingue deux cas: récupération des éléments d'un utilisateur et récupération des éléments partagés avec un utilisateur
+     * - Dans le 1er cas (isOwner = 1), on retourne les infos de l'élément et du refElement
+     * - Dans le second cas (isOwner = 0), on retourne le droit, le refRight, l'élément, le refElement et le propriétaire
+     * - Gestion des erreurs
+     * @author Alban Truc
+     * @param string|MongoId $idUser
+     * @param string $isOwner
+     * @since 01/05/2014
+     * @return array
+     */
+
+    public function returnElementsDetails($idUser, $isOwner)
+    {
+        if($isOwner == '1')
+        {
+            $criteria = array(
+                'state' => (int)1,
+                'idOwner' => $idUser
+            );
+
+            $elements = self::find($criteria);
+
+            //récupération des refElement pour chaque élément
+            foreach($elements as $key => $element)
+            {
+                unset($element['idOwner']);
+
+                $refElementManager = new RefElementManager();
+                $refElement = $refElementManager->findById($element['idRefElement']);
+
+                unset($element['idRefElement']);
+
+                if(is_array($refElement) && !(array_key_exists('error', $refElement)))
+                {
+                    $element['refElement'] = $refElement;
+                    $elements[$key] = $element;
+                }
+                else unset($elements[$key]);
+            }
+
+            if(empty($elements))
+                return array('error' => 'No match found.');
+            else
+                return $elements;
+        }
+        else if($isOwner == '0')
+        {
+            return self::returnSharedElementsDetails($idUser);
+        }
+        else return array('error' => 'Parameter isOwner must be 0 or 1');
+    }
+
+    /**
+     * Retourne le droit, le refRight, l'élément, le refElement et le propriétaire
+     * @author Alban Truc
+     * @param string|MongoId $idUser
+     * @since 01/05/2014
+     * @return array
+     */
+
+    public function returnSharedElementsDetails($idUser)
+    {
+        $criteria = array(
+            'state' => (int)1,
+            'idUser' => $idUser
+        );
+
+        //récupération des droits sur les éléments
+        $rightManager = new RightManager();
+        $rights = $rightManager->find($criteria);
+
+        $userManager = new UserManager();
+        $refRightManager = new RefRightManager();
+        $refElementManager = new RefElementManager();
+
+        //pour chaque droit
+        foreach($rights as $key => $right)
+        {
+            $owner = NULL;
+            $refRight = NULL;
+
+            //Récupération de l'élément. On enlève le droit de la liste si l'élément n'est pas disponible
+            $elementCriteria = array(
+                '_id' => new MongoId($right['idElement']),
+                'state' => (int)1
+            );
+
+            unset($right['idElement']);
+            $element = self::findOne($elementCriteria);
+            $idOwner = $element['idOwner'];
+            unset($element['idOwner']);
+
+            if(is_array($element) && !(array_key_exists('error', $element)))
+            {
+                //récupération du refElement. On enlève le droit de la liste si le refElement n'est pas trouvé
+                $refElement = $refElementManager->findById($element['idRefElement']);
+                unset($element['idRefElement']);
+
+                if(is_array($refElement) && !(array_key_exists('error', $refElement)))
+                {
+                    $element['refElement'] = $refElement;
+                    $right['element'] = $element;
+                }
+                else
+                {
+                    unset($rights[$key]);
+                    break;
+                }
+            }
+            else
+            {
+                unset($rights[$key]);
+                break;
+            }
+
+            /*
+             * Récupération de l'utilisateur propriétaire.
+             * Si son état n'est pas à 1, le droit n'aurait pas du être à 1; donc on enlève ce droit de la liste.
+             */
+            $userCriteria = array(
+                '_id' => new MongoId($idOwner),
+                'state' => (int)1
+            );
+
+            $fieldsToReturn = array(
+                'firstName' => 1,
+                'lastName' => 1
+            );
+
+            unset($right['idUser']);
+
+            $owner = $userManager->findOne($userCriteria, $fieldsToReturn);
+
+            if(is_array($owner) && !(array_key_exists('error', $owner)))
+            {
+                $right['owner'] = $owner;
+            }
+            else
+            {
+                unset($rights[$key]);
+                break;
+            }
+
+            //Récupération du refRight. S'il n'existe pas on enlève ce droit de la liste.
+            $refRight = $refRightManager->findById($right['idRefRight']);
+            unset($right['idRefRight']);
+
+            if(is_array($refRight) && !(array_key_exists('error', $refRight)))
+            {
+                $right['refRight'] = $refRight;
+            }
+            else
+            {
+                unset($rights[$key]);
+                break;
+            }
+
+            $rights[$key] = $right;
+        }
+
+        if(empty($rights))
+            return array('error' => 'No match found.');
+        else
+            return $rights;
     }
 }
